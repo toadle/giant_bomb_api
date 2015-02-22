5ecaec00d5a0d554db6a2edec7f5dc09aadbc1b6
Tim Adler <me@tim-adler.com>
2015-02-22 23:54:55 +0100

    added company and character as collections

--- lib/giant_bomb_api/resource/character.rb
+++ lib/giant_bomb_api/resource/character.rb
@@ -1,6 +1,22 @@
 module GiantBombApi
   class Resource::Character
-    extend GiantBombApi::Resource
+    extend GiantBombApi::CollectionResource
+
+    resource_attribute :aliases
+    resource_attribute :api_detail_url
+    resource_attribute :birthday
+    resource_attribute :date_added
+    resource_attribute :date_last_updated
+    resource_attribute :deck
+    resource_attribute :description
+    resource_attribute :gender
+    resource_attribute :id
+    resource_attribute :image, resource_name: :image
+    resource_attribute :last_name
+    resource_attribute :name
+    resource_attribute :real_name
+    resource_attribute :site_detail_url
+    resource_attribute :first_appeared_in_game, resource_name: :game
 
     private
 
--- lib/giant_bomb_api/resource/company.rb
+++ lib/giant_bomb_api/resource/company.rb
@@ -1,6 +1,25 @@
 module GiantBombApi
   class Resource::Company
-    extend GiantBombApi::Resource
+    extend GiantBombApi::CollectionResource
+
+    resource_attribute :abbreviation
+    resource_attribute :aliases
+    resource_attribute :api_detail_url
+    resource_attribute :date_added
+    resource_attribute :date_founded
+    resource_attribute :date_last_updated
+    resource_attribute :deck
+    resource_attribute :description
+    resource_attribute :id
+    resource_attribute :image, resource_name: :image
+    resource_attribute :location_address
+    resource_attribute :location_city
+    resource_attribute :location_country
+    resource_attribute :location_state
+    resource_attribute :name
+    resource_attribute :phone
+    resource_attribute :site_detail_url
+    resource_attribute :website
 
     private
 
